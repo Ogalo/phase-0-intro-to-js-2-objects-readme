ff6232829743b84a1441c92ea0b42ed39a07ebdd
Otieno Harrison Omondi <ogaloharrison@gmail.com>
2022-06-05 00:35:18 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+const wednesdayMenu = {
+  cheesePlate: {
+    soft: "Brie",
+    semiSoft: "Fontina",
+    hard: "Provolone",
+  },
+  fries: "Sweet potato",
+  salad: "Southwestern",
+};
+
+console.log(Object.keys(wednesdayMenu.cheesePlate));
